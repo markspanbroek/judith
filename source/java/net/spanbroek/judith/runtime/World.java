98a917118520b2305f37ace6184810ba55220171
Mark Spanbroek <mark@spanbroek.net>
2009-11-02 15:14:13 +0000

    Refactoring.
    git-svn-id: https://subversion.spanbroek.net/judith@84 b57a26ab-4e2e-0410-ade9-a67858e2592c

--- source/java/net/spanbroek/judith/runtime/World.java
+++ source/java/net/spanbroek/judith/runtime/World.java
@@ -41,6 +41,7 @@ public class World extends Scope {
         interpret("parsing/ParserGenerator.judith");
         interpret("parsing/ParserGenerator-empty.judith");
         interpret("parsing/ParserGenerator-begin.judith");
+        interpret("parsing/ParserGenerator-end.judith");
         interpret("parsing/basics.judith");
         interpret("parsing/astext.judith");
         interpret("parsing/memoization.judith");
