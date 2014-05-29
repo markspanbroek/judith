195b802076d13dfc635ef148eb1d85f09d1f6dc1
Mark Spanbroek <mark.spanbroek@philips.com>
2014-05-29 22:55:04 +0200

    Simplify parsing of numbers and text

--- source/java/net/spanbroek/judith/parser/ExpressionParser.java
+++ source/java/net/spanbroek/judith/parser/ExpressionParser.java
@@ -2,7 +2,7 @@ package net.spanbroek.judith.parser;
 
 import net.spanbroek.judith.tree.Boolean;
 import net.spanbroek.judith.tree.Text;
-import net.spanbroek.parsing.Position;
+import net.spanbroek.parsing.Context;
 import net.spanbroek.parsing.Rule;
 import net.spanbroek.parsing.Transformation;
 
@@ -36,19 +36,17 @@ public class ExpressionParser extends Rule {
     private void setupTransformations() {
         boolean_.transform(new Transformation() {
             @Override
-            public Object transform(List<Object> objects, Position position) {
+            public Object transform(List<Object> objects, Context context) {
                 boolean value = objects.get(0).equals("true");
                 return new Boolean(value);
             }
         });
         text.transform(new Transformation() {
             @Override
-            public Object transform(List<Object> objects, Position position) {
-                StringBuilder result = new StringBuilder();
-                for (int i=1; i<objects.size()-1; i++) {
-                    result.append(objects.get(i));
-                }
-                return new Text(result.toString());
+            public Object transform(List<Object> objects, Context context) {
+                String originalText = context.getOriginalText();
+                String value = originalText.substring(1, originalText.length() - 1);
+                return new Text(value);
             }
         });
     }
